cf0c1ccf07ffc70429d634a338eaa738304fae6c
Demian Katz <demian.katz@villanova.edu>
2020-11-02 16:53:01 -0500

    Resend failed batches.

--- bin/send-batch.php
+++ bin/send-batch.php
@@ -10,40 +10,61 @@ $container = require __DIR__ . '/../config/container.php';
 $messageTable = $container->get(Table\Message::class);
 $batchTable = $container->get(Table\Batch::class);
 
+// Let's check for failed batches from prior runs:
+$unsentBatches = array_map(
+    function ($batch) {
+        return $batch['id'];
+    }, $batchTable->select(['sent' => 0])->toArray()
+);
+
 $messages = $messageTable->select(['batch_id' => null])->toArray();
 
 if (count($messages) == 0) {
-    echo "No messages to forward.\n";
-    exit(0);
-}
+    // No messages to send? Set up an empty placeholder batch in case we still need to resend failed batches:
+    $batch = null;
+} else {
+    // If we got this far, we have messages, so let's create a batch:
+    $batchTable->insert(['sent' => 0]);
+    $batch = $batchTable->select(['id' => $batchTable->getLastInsertValue()])->toArray()[0];
 
-// If we got this far, we have messages, so let's create a batch:
-$batchTable->insert(['sent' => 0]);
-$batch = $batchTable->select(['id' => $batchTable->getLastInsertValue()])->toArray()[0];
+    $where = function ($sql) use ($messages) {
+        $messageIds = array_map(
+            function ($arr) {
+                return $arr['id'];
+            }, $messages
+        );
+        $sql->where->in('id', $messageIds)
+            ->isNull('batch_id');
+    };
+    $count = $messageTable->update(['batch_id' => $batch['id']], $where);
 
-$where = function ($sql) use ($messages) {
-    $messageIds = array_map(
-        function ($arr) {
-            return $arr['id'];
-        }, $messages
-    );
-    $sql->where->in('id', $messageIds)
-        ->isNull('batch_id');
-};
-$count = $messageTable->update(['batch_id' => $batch['id']], $where);
-
-// If something weird happened and we updated an unexpected number of rows, we should
-// reload the messages:
-if ($count != count($messages)) {
-    echo "WARNING: message count mismatch; reloading data!\n";
-    $messages = $messageTable->select(['batch_id' => $batch['id']])->toArray();
+    // If something weird happened and we updated an unexpected number of rows, we should
+    // reload the messages:
+    if ($count != count($messages)) {
+        echo "WARNING: message count mismatch; reloading data!\n";
+        $messages = $messageTable->select(['batch_id' => $batch['id']])->toArray();
+    }
 }
 
 // Send the batch
 $forwarder = $container->get(MessageForwarder::class);
-$forwarder->forward($messages, $batch['id']);
+$forwarder->forward($messages, $batch['id'] ?? null, $unsentBatches);
 
 // Mark the batch as sent
-$batchTable->update(['sent' => 1], ['id' => $batch['id']]);
-echo "Sent batch {$batch['id']} with {$count} messages.\n";
+$sentBatches = $unsentBatches;
+if (isset($batch['id'])) {
+    $sentBatches[] = $batch['id'];
+}
+$updateCallback = function ($sql) use ($sentBatches) {
+    $sql->where->in('id', $sentBatches);
+};
+$batchTable->update(['sent' => 1], $updateCallback);
+if (!empty($unsentBatches)) {
+    echo "Resent failed batch(es): " . implode(', ', $unsentBatches) . "\n";
+}
+if (isset($batch['id'])) {
+    echo "Sent batch {$batch['id']} with {$count} message(s).\n";
+} else {
+    echo "No new messages to send.\n";
+}
 exit(0);
--- src/App/src/MessageForwarder.php
+++ src/App/src/MessageForwarder.php
@@ -5,6 +5,7 @@ use Laminas\Http\Client;
 
 class MessageForwarder
 {
+    protected $blankMessage = '{"@context":"https://schema.org/extensions","@type":"MessageCard","themeColor":"0072C6","title":"VuOwma Message","text":""}';
     protected $client;
     protected $vuowmaUrl;
     protected $webhookUrl;
@@ -19,11 +20,18 @@ class MessageForwarder
         $this->client = $options['client'] ?? new Client();
     }
 
-    public function forward(array $messages, $batch_id)
+    public function forward(array $messages, $batch_id, array $unsentBatches)
     {
-        $message = json_decode($messages[0]['data'], true);
+        if (empty($messages) && empty($unsentBatches)) {
+            // nothing to do:
+            return;
+        }
+        $message = json_decode($messages[0]['data'] ?? $this->blankMessage, true);
+        foreach ($unsentBatches as $unsentBatch) {
+            $message['text'] = "Resending previously failed [batch $unsentBatch](" . $this->vuowmaUrl . '?batch=' . $unsentBatch . ")  \n" . $message['text'];
+        }
         if (count($messages) > 1) {
-            $message['text'] = count($messages) . " log messages in [batch $batch_id](" . $this->vuowmaUrl . '?batch=' . $batch_id . ")\nFirst message: " . $message['text'];
+            $message['text'] = count($messages) . " log messages in [batch $batch_id](" . $this->vuowmaUrl . '?batch=' . $batch_id . ")  \nFirst message: " . $message['text'];
         }
         $this->client->setUri($this->webhookUrl);
         $this->client->setMethod('POST');
